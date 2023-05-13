077c83d4e6d09fa12491291f553dc89b5819d63a
Mohammad Hosein Rastegarinia <mh.rastegari@outlook.com>
2023-05-14 02:22:26 +0330

    Add neutral endpoint sample controller

---
+++ src/VersioningAPISample/Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+﻿namespace VersioningAPISample.Controllers;
+
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiController]
+[ApiVersionNeutral]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    [Route("ping")]
+    public IActionResult Ping()
+    {
+        return Ok("All good!");
+    }
+}
